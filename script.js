435bf9add074a2244cc955000be45c2c3364d2ab
mk <k3042227625n@gmail.com>
2021-10-29 14:53:47 +0900

    second commit

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++)
-//モダールを開く
-btnOpenModal[i].addEventListener('click', function() {
+const openModal = function () {
     console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-});
+}
 
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+//モダールを開く
+    btnOpenModal[i].addEventListener('click', openModal);
 //モダールを×で閉じる
-btnCloseModal.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-})
+btnCloseModal.addEventListener('click', closeModal);
 //モダールをウィンドウの外で閉じる
-overlay.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-})
+overlay.addEventListener('click', closeModal);
